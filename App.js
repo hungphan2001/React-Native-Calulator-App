5d3037083a1ec9cfc8635796a6853a7551ee1470
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-11-29 21:38:03 +0700

    Implementing Dark Mode
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- App.js
+++ App.js
@@ -4,33 +4,34 @@
  *
  * @format
  * @flow strict-local
+ * @author hungphan2001
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {};
+import { useState } from 'react';
+import { ScrollView, Text, StatusBar, StyleSheet, Switch, View } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import {myColors} from './src/styles/Colors';
+ const App = () => {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+    <View style={theme === 'light' ? styles.container : [styles.container, { backgroundColor: 'black' }]}>
+      <StatusBar styles="auto"></StatusBar>
+      <Switch
+        value={theme === 'dark'}
+        onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      />
+    </View>
+    </ThemeContext.Provider>
+  );
+};
 
 const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
